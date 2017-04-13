527a26a0f3dc221d201a3bf2750acb5e5bb4e442
dsantial <gaoxd126@gmail.com>
2017-04-13 08:54:54 +0800

    多态的经典示例

---
+++ src/code/Test7.java
@@ -0,0 +1,16 @@
+package code;
+
+/**
+ * Created by 390767 on 17-4-13.
+ */
+
+class Test5 {int i = 1;}
+
+
+class Test6 {
+    int i= 2;
+
+    public static void main(String[] args) {
+        System.out.println("main method");
+    }
+}
